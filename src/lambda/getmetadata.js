4cff9581b9ec82af8be865d4e385ba3f6ab30e38
Ethan <me@ethan.link>
2018-11-18 10:27:39 +1100

    Added get metadata cloud function

---
+++ src/lambda/getmetadata.js
@@ -0,0 +1,36 @@
+const headers = {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+};
+
+exports.handler = async (event, context, callback) => {
+    
+    if (event.httpMethod === 'OPTIONS') { // The browser is checking the function to see the headers (called 'preflight' I think)
+        callback(null, {statusCode: 204,headers});
+    }
+    if (event.httpMethod !== 'POST') {
+        callback({statusCode: 405,headers}, null);
+    }
+    
+    var body = JSON.parse(event.body);
+    
+    const globals = require('../globals.js')(body.testing === true); // GLOBAL VARIABLES    
+    const stripe = require('stripe')(process.env[`STRIPE_${body.testing === true ? 'TEST_' : ''}SECRET_KEY`]);
+    
+    return stripe.customers.list({email: body.email})
+    .then(res => {
+        var customersWithEmail = res.data;
+        if(customersWithEmail.length > 0) {
+            if(customersWithEmail[0].metadata.signinCode === body.code) {
+                var metadata = customersWithEmail[0].metadata;
+                delete metadata.signinCode;
+                callback(null, {statusCode: 200,headers,body: JSON.stringify(metadata)});
+            } else {
+                callback(null, {statusCode: 401,headers,body: JSON.stringify({code: 'WRONG_AUTH_CODE'})});
+            }
+        } else {
+            callback(null, {statusCode: 404,headers,body: JSON.stringify({code: 'USER_NOT_FOUND'})});
+        }
+    });
+}
