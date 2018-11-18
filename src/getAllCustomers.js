6aa7ac324d6188c414ef3436f48713406c258ee1
Ethan <me@ethan.link>
2018-11-18 18:30:00 +1100

    FIXED EXTREMELY EVIL STRIPE PAGINATION BUG

--- src/getAllCustomers.js
+++ src/getAllCustomers.js
@@ -1,9 +1,15 @@
 module.exports = function(stripe) {
+    var customers = [];
     var resHandler = function(res) {
+        if(typeof res.data !== 'undefined') {
+            for(var i = 0; i < res.data.length; i++) {
+                customers.push(res.data[i]);
+            }
+        }
         if(!res.has_more) {
-            return res;
+            return {data: customers};
         } else {
-            return stripe.customers.list({limit: 100, starting_after: res.reverse()[0].id})
+            return stripe.customers.list({limit: 100, starting_after: res.data.reverse()[0].id})
             .then(resHandler)
         }
     };
