81385898ec6532aba368f8c4c8b7e5abb213d444
Ethan <me@ethan.link>
2018-11-18 12:01:36 +1100

    Make Stripe pagination actually work (no way to test though until I get > 100 users)

--- src/getAllCustomers.js
+++ src/getAllCustomers.js
@@ -1,3 +1,12 @@
 module.exports = function(stripe) {
+    var resHandler = function(res) {
+        if(!res.has_more) {
+            return res;
+        } else {
+            return stripe.customers.list({limit: 100, starting_after: res.reverse()[0].id})
+            .then(resHandler)
+        }
+    };
     return stripe.customers.list({limit: 100})
+    .then(resHandler)
 }
