7645c00587f33ad947a32a4188bedfc1c6f0d7e6
Ethan <me@ethan.link>
2019-07-13 13:39:02 +0000

    Added DOMPurify

--- src/lambda/getpageinfo.js
+++ src/lambda/getpageinfo.js
@@ -21,6 +21,7 @@ exports.handler = async (event, context, callback) => {
     const humanizeDuration = require('humanize-duration');
     const moment = require('moment');
     const marked = require('marked');
+    const DOMPurify = require('dompurify');
     
     return stripe.customers.list({email: body.username + '@username.maker.rocks'})
     .then(res => {
@@ -224,7 +225,7 @@ exports.handler = async (event, context, callback) => {
                 pageInfo.profileHue = metadata.profileHue;
             }
             if(typeof metadata.bio == 'string' && metadata.bio.length > 0){
-                pageInfo.bio = marked(metadata.bio).replace(/\n/g, '<br>');
+                pageInfo.bio = marked(DOMPurify.sanitize(metadata.bio)).replace(/\n/g, '<br>');
             }
             
             return Promise.all(promiseChains)
